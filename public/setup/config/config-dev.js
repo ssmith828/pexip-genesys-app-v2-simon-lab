7c659f1e68af712cf7571edf93001f558f0a67af
Simon Smith <71359986+ssmith828@users.noreply.github.com>
2026-01-07 13:34:28 -0500

    Delete public directory

--- public/setup/config/config-dev.js
+++
@@ -1,5 +0,0 @@
-export default {
-  clientID: 'fa593b6d-c2f1-40a7-8a8f-a26fe7575f16',
-  wizardUriBase: 'https://localhost:3000/pexip-genesys-app-example/setup/index.html',
-  premiumAppIntegrationTypeId: 'premium-app-example'
-}
