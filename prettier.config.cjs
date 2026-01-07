3d15899230dfbc86a147e9865ecc7678c9289209
Simon Smith <71359986+ssmith828@users.noreply.github.com>
2026-01-07 13:15:09 -0500

    Add files via upload

---
+++ prettier.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  arrowParens: 'always',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  printWidth: 80,
+  proseWrap: 'always',
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'none',
+  useTabs: false
+}
